ca65e0b477637485044c0e14afb81e1050feb6bc
linmcgee <medi61200@mail.ru>
2020-04-14 00:36:28 +0300

    Попап

---
+++ js/form-index.js
@@ -0,0 +1,14 @@
+var form = document.querySelector(".popup");
+var call = document.querySelector(".call-button");
+
+var close = popup.querySelector(".popup-close");
+
+call.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("popup-visible");
+  }); 
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("popup-visible");
+  });
