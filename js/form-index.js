144f5b4241d40fbd8dbe28989e59b7d44f2c0649
linmcgee <medi61200@mail.ru>
2020-04-15 00:42:13 +0300

    Попап JS

--- js/form-index.js
+++ js/form-index.js
@@ -1,14 +1,24 @@
 var popup = document.querySelector(".popup");
 var call = document.querySelector(".call-button");
 
-var close = popup.querySelector(".popup-close");
+var close = document.querySelector(".close");
 
 call.addEventListener("click", function (evt) {
     evt.preventDefault();
-    popup.classList.add("popup-visible");
+    popup.classList.toggle("popup-visible");
   }); 
 
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("popup-visible");
   });
+
+
+  window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup.classList.contains("popup-visible")) {
+          popup.classList.remove("popup-visible");
+        }
+    }
+});
